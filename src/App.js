06fbf2a576f773ebe99c0b1cd1c6e0a8be15a42b
spgathade <subodh.gathade@moderncoe.edu.in>
2021-04-21 22:15:37 +0530

    added styling to the game

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import Board from './Components/Board';
 
+import './Styles/Board.style.scss';
+
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <h1>X-O Tic Tac Toe</h1>
       <Board />
     </div>
